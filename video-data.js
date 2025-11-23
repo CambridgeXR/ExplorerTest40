7a02a14602350fdd0515972424a9a01e127babed
CambridgeXR <chris.macdonald@lucy.cam.ac.uk>
2025-11-23 18:06:25 +0000

    Create video-data.js

---
+++ video-data.js
@@ -0,0 +1,18 @@
+const audienceVideos = {
+    "Audience of 0": { cloudflare: "https://pub-850a8ef9d4f34159a20706a415caaaf8.r2.dev/N0-CF.h264.mp4", youtube: "https://youtube.com/watch?v=8D5ZJjV-9No" },
+    "Audience of 1": { cloudflare: "https://pub-850a8ef9d4f34159a20706a415caaaf8.r2.dev/N1-CF.h264.mp4", youtube: "https://youtube.com/watch?v=L61TY-lVWYc" },
+    "Audience of 2": { cloudflare: "https://pub-850a8ef9d4f34159a20706a415caaaf8.r2.dev/N2-CF.h264.mp4", youtube: "https://www.youtube.com/watch?v=jisLJabMOaw" },
+    "Audience of 4": { cloudflare: "https://pub-850a8ef9d4f34159a20706a415caaaf8.r2.dev/N4-CF.h264.mp4", youtube: "https://www.youtube.com/watch?v=583YioSTgGE" },
+    "Audience of 9": { cloudflare: "https://pub-850a8ef9d4f34159a20706a415caaaf8.r2.dev/N9-CF.h264.mp4", youtube: "https://www.youtube.com/watch?v=7AS7NEA9PFo" },
+    "Audience of 16": { cloudflare: "https://pub-850a8ef9d4f34159a20706a415caaaf8.r2.dev/N16-CF.h264.mp4", youtube: "https://www.youtube.com/watch?v=_y-lSw1SNaU" },
+    "Audience of 25": { cloudflare: "https://pub-850a8ef9d4f34159a20706a415caaaf8.r2.dev/N25-CF.h264.mp4", youtube: "https://www.youtube.com/watch?v=CBBwXbX5ogE" },
+    "Audience of 50": { cloudflare: "https://pub-850a8ef9d4f34159a20706a415caaaf8.r2.dev/N50-CF.h264.mp4", youtube: "https://www.youtube.com/watch?v=RUnKCfWFSfI" },
+    "Audience of 100": { cloudflare: "https://pub-850a8ef9d4f34159a20706a415caaaf8.r2.dev/N100-CF.h264.mp4", youtube: "https://www.youtube.com/watch?v=ww986pYF46E" },
+    "Audience of 500": { cloudflare: "https://pub-850a8ef9d4f34159a20706a415caaaf8.r2.dev/N500-CF.h264.mp4", youtube: "https://www.youtube.com/watch?v=S3Ve7FGfjWk" },
+    "Audience of 1000s": { cloudflare: "https://pub-850a8ef9d4f34159a20706a415caaaf8.r2.dev/N10K-CF.h264.mp4", youtube: "https://www.youtube.com/watch?v=jyUNFPBmJkA" }
+};
+const scenarioVideos = {
+    "TV teleprompter": { cloudflare: "https://pub-850a8ef9d4f34159a20706a415caaaf8.r2.dev/ACC01-CF.h264.mp4", youtube: "https://www.youtube.com/watch?v=3ZNddx_YNc0" },
+    "Job interview": { cloudflare: "https://pub-850a8ef9d4f34159a20706a415caaaf8.r2.dev/JIC01-CF.h264.mp4", youtube: "https://www.youtube.com/watch?v=-tsxJfabw0s" },
+    "Radio interview": { cloudflare: "https://pub-850a8ef9d4f34159a20706a415caaaf8.r2.dev/RIC01-CF.h264.mp4", youtube: "https://www.youtube.com/watch?v=sE3pahN4BwI" }
+};
